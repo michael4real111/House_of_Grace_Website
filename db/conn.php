ddc9ffa17fa905c1ad883796213365f61ab3b8fa
michael4real111 <michael4real111@gmail.com>
2023-12-18 13:15:53 -0500

    database switch

--- db/conn.php
+++ db/conn.php
@@ -1,16 +1,16 @@
 <?php
     //Development connection
-    $host = '127.0.0.1';
-    $db = 'weddings_db';
-    $user = 'root';
-    $pass = '';
-    $charset = 'utf8mb4';
+    //$host = '127.0.0.1';
+    //$db = 'weddings_db';
+    //$user = 'root';
+    //$pass = '';
+    //$charset = 'utf8mb4';
 
-    //$host = 'sql302.infinityfree.com';
-    //$db = 'if0_35614255_db_houseofgrace';
-    //$user = 'if0_35614255';
-    //$pass = 'dOUNPCJTrJ';  
-    //$charset = 'utf8mb4'; 
+    $host = 'sql302.infinityfree.com';
+    $db = 'if0_35614255_db_houseofgrace';
+    $user = 'if0_35614255';
+    $pass = 'dOUNPCJTrJ';  
+    $charset = 'utf8mb4'; 
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
